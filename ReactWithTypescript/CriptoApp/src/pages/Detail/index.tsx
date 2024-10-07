40ea47cd2e4b756cd99577c6758a1803bf2d3f20
maltanv <gustavomaltanv@gmail.com>
2024-10-07 16:26:55 -0300

    chore: finalize CriptoApp

--- ReactWithTypescript/CriptoApp/src/pages/Detail/index.tsx
+++ ReactWithTypescript/CriptoApp/src/pages/Detail/index.tsx
@@ -1,7 +1,99 @@
+import { useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import { CoinProps } from "../Home";
+import styles from './detail.module.css';
+
+interface ResponseData {
+  data: CoinProps
+}
+
+interface ErrorData {
+  error: string
+}
+
+type DataProps = ResponseData | ErrorData;
+
 export function Detail() {
+  const { cripto } = useParams();
+  const navigate = useNavigate();
+
+  const [coin, setCoin] = useState<CoinProps>();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function getCoin() {
+      try {
+        fetch(`https://api.coincap.io/v2/assets/${cripto}`)
+          .then(res => res.json())
+          .then((data: DataProps) => {
+            if ("error" in data) {
+              navigate("/");
+              return;
+            }
+
+            const price = Intl.NumberFormat("en-US", {
+              style: "currency",
+              currency: "USD"
+            });
+
+            const priceCompact = Intl.NumberFormat("en-US", {
+              style: "currency",
+              currency: "USD",
+              notation: "compact"
+            });
+
+            const resultData = {
+              ...data.data,
+              formatedPrice: price.format(Number(data.data.priceUsd)),
+              formatedMarket: priceCompact.format(Number(data.data.marketCapUsd)),
+              formatedVolume: priceCompact.format(Number(data.data.volumeUsd24Hr))
+            };
+
+            setCoin(resultData);
+            setLoading(false);
+          })
+      } catch (error) {
+        console.log(error);
+        navigate("/");
+      }
+    }
+
+    getCoin();
+  }, [cripto]);
+
+  if (loading || !coin) return (
+    <div className={styles.container}>
+      <h4 className={styles.center}>Carregando detalhes...</h4>
+    </div>
+  );
+
   return (
-    <main>
-      <h1>página detalhes</h1>
+    <main className={styles.container}>
+      <h1 className={styles.center}>{coin?.name} </h1>
+      <h1 className={styles.center}>{coin?.symbol} </h1>
+
+      <section className={styles.content} >
+        <img className={styles.logo} src={`https://assets.coincap.io/assets/icons/${coin?.symbol.toLowerCase()}@2x.png`} alt={`Logo ${cripto}`} />
+
+        <h1>{coin?.name} | {coin?.symbol}</h1>
+
+        <p><strong>Preço: </strong>{coin?.formatedPrice}</p>
+
+        <a>
+          <strong>Mercado: </strong>{coin?.formatedMarket}
+        </a>
+
+        <a>
+          <strong>Volume: </strong>{coin?.formatedVolume}
+        </a>
+
+        <a>
+          <strong>Mudança 24h: </strong><span className={Number(coin?.changePercent24Hr) > 0 ? styles.protift : styles.loss} >{Number(coin?.changePercent24Hr).toFixed(4)}</span>
+        </a>
+
+
+      </section>
+
     </main>
   )
 }
--- ReactWithTypescript/CriptoApp/src/pages/Home/index.tsx
+++ ReactWithTypescript/CriptoApp/src/pages/Home/index.tsx
@@ -1,17 +1,104 @@
 import styles from './Home.module.css';
 import { BsSearch } from 'react-icons/bs';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+
+import { useState, FormEvent, useEffect } from 'react';
+
+export interface CoinProps {
+  id: string;
+  name: string;
+  symbol: string;
+  priceUsd: string;
+  vwap24Hr: string;
+  changePercent24Hr: string;
+  rank: string;
+  supply: string;
+  maxSupply: string;
+  marketCapUsd: string;
+  volumeUsd24Hr: string;
+  explorer: string;
+
+  formatedPrice?: string;
+  formatedMarket?: string;
+  formatedVolume?: string;
+}
+
+interface DataProps {
+  data: CoinProps[]
+}
 
 export function Home() {
+  const [input, setInput] = useState("");
+  const [coins, setCoins] = useState<CoinProps[]>([]);
+  const navigate = useNavigate();
+  const limit = 10;
+  const [offset, setOffset] = useState<number>(0);
+
+  useEffect(() => {
+    getData();
+  }, [offset])
+
+  async function getData() {
+    fetch(`https://api.coincap.io/v2/assets?limit=10&offset=${offset}`)
+      .then(res => res.json())
+      .then((data: DataProps) => {
+        const coinsData = data.data;
+
+        const price = Intl.NumberFormat("en-US", {
+          style: "currency",
+          currency: "USD"
+        });
+
+        const priceCompact = Intl.NumberFormat("en-US", {
+          style: "currency",
+          currency: "USD",
+          notation: "compact"
+        });
+
+        const formatedResult = coinsData.map((item) => {
+          const formated = {
+            ...item,
+            formatedPrice: price.format(Number(item.priceUsd)),
+            formatedMarket: priceCompact.format(Number(item.marketCapUsd)),
+            formatedVolume: priceCompact.format(Number(item.volumeUsd24Hr))
+          };
+
+          return formated;
+        });
+        const listCoins = [...coins, ...formatedResult];
+        setCoins(listCoins);
+      });
+
+  }
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+
+    if (input === "") return;
+
+    navigate(`/detail/${input}`);
+  }
+
+  function handleGetMore() {
+    if (offset === 0) {
+      setOffset(limit);
+      return;
+    }
+    setOffset(offset + limit);
+
+  }
+
   return (
     <main className={styles.container}>
-      <form className={styles.form} >
+      <form className={styles.form} onSubmit={handleSubmit} >
         <input
           type="text"
           placeholder='Procure sua moeda...'
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
         />
         <button type='submit' >
-          <BsSearch size={30} color='#fff'/>
+          <BsSearch size={30} color='#fff' />
         </button>
       </form>
 
@@ -26,37 +113,50 @@ export function Home() {
           </tr>
         </thead>
 
-        <tbody id="tbody"> 
-          <tr className={styles.tr}>
+        <tbody id="tbody">
+          {
+            coins.length > 0 && coins.map((item) => (
+              <tr className={styles.tr} key={item.id}>
+                <td className={styles.tdLabel} data-label='Moeda'>
+                  <div className={styles.name} >
+                    <img 
+                      className={styles.logo}
+                      alt={`Logo ${item.name}`}
+                      src={`https://assets.coincap.io/assets/icons/${item.symbol.toLowerCase()}@2x.png`}
+                    />
+                    <Link to={`/detail/${item.id}`}>
+                      <span>{item.name}</span> | {item.symbol}
+                    </Link>
+                  </div>
+                </td>
 
-            <td className={styles.tdLabel} data-label='Moeda'>
-              <div className={styles.name} >
-                <Link to={'/detail/'}>
-                  <span>Bitcoin</span> | BTC
-                </Link>
-              </div>
-            </td>
+                <td className={styles.tdLabel} data-label='Valor mercado'>
+                  {item.formatedMarket}
+                </td>
 
-            <td className={styles.tdLabel} data-label='Valor mercado'>
-              1T
-            </td>
+                <td className={styles.tdLabel} data-label='Preço'>
+                  {item.formatedPrice}
+                </td>
 
-            <td className={styles.tdLabel} data-label='Preço'>
-              8.000
-            </td>
+                <td className={styles.tdLabel} data-label='Volume'>
+                  {item.formatedVolume}
+                </td>
 
-            <td className={styles.tdLabel} data-label='Volume'>
-              2B
-            </td>
-
-            <td className={styles.tdLabel} data-label='Mudança 24h'>
-              <span className={styles.profit}> 2.5 </span>
-            </td>
-
-          </tr>
+                <td className={styles.tdLabel} data-label='Mudança 24h'>
+                  <span className={Number(item.changePercent24Hr) > 0? styles.profit : styles.loss}>
+                    {Number(item.changePercent24Hr).toFixed(4)} 
+                  </span>
+                </td>
+              </tr>
+            ))
+          }
         </tbody>
-
       </table>
+
+      <button className={styles.buttonMore} onClick={handleGetMore}>
+        Carregar mais
+      </button>
+
     </main>
   )
 }
